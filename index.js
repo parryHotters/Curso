dc4129bd0b41290aa07f254545088c615ea9bd26
YoGhe~ <HOTTERSPARRY@GMAIL.COM>
2023-01-27 19:31:21 -0400

    PRUEBA2

---
+++ index.js
@@ -0,0 +1,15 @@
+{
+    nombreCliente;
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+}
+
+const nombreCliente = "leonardo";
+const dniCliente = "13804050";
+const numeroCuenta = "123434343";
+const saldoCuenta = 1000;
+
+
+
+console.log(nombreCliente);
