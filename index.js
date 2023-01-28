2569f718f268157580ad83783565ffbedd1e37d2
YoGhe~ <HOTTERSPARRY@GMAIL.COM>
2023-01-27 22:19:44 -0400

    PRUEBAAAAAAA

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
+/*definicion de clases*/
+
 class cliente {
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
     rutCliente;
 }
- const cliente1 =new cliente();
-cliente1.nombreCliente = "Jose";
-cliente1.dniCliente = "13232";
-cliente1.numeroCuenta = "987897722";
-cliente1.saldoCuenta = 2000;
 
-const cliente2 =new cliente();
-cliente2.nombreCliente = "Leonardo";
-cliente2.dniCliente = "13232";
-cliente2.numeroCuenta = "987897722";
-cliente2.saldoCuenta = 2000;
+class CuentaCorriente {
+    numero;
+    saldo;
+    agencia;
 
-const cliente3 =new cliente();
-cliente3.nombreCliente = "Maria";
-cliente3.dniCliente = "132778";
-cliente3.numeroCuenta = "987822";
-cliente3.saldoCuenta = 2222;
+    depositoEnCuenta(valor) {
+        this.saldo += valor;
 
+    }
 
+    retirarDeCuenta (valor) {
+        this.saldo -= valor;
+    }
+}
 
-const nombreCliente = "leonardo";
-const dniCliente = "13804050";
-const numeroCuenta = "123434343";
-const saldoCuenta = 1000;
+cuentaDeLeonardo =new CuentaCorriente();
+cuentaDeLeonardo.saldo =0;
 
+cuentaDeLeonardo.depositoEnCuenta(100);
 
+cuentaDeLeonardo.retirarDeCuenta(80);
 
-console.log(nombreCliente);
-
-console.log(cliente1);
-console.log(cliente2);
-console.log(cliente3);
+console.log(cuentaDeLeonardo);
