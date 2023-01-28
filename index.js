03879e61ad036a1d1eca1d4c2ffa0784b5b3a3c2
YoGhe~ <HOTTERSPARRY@GMAIL.COM>
2023-01-27 20:39:48 -0400

    Primera C V

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-/*{
+class cliente {
     nombreCliente;
     dniCliente;
     numeroCuenta;
     saldoCuenta;
-}*/
+    rutCliente;
+}
+ const cliente1 =new cliente();
+cliente1.nombreCliente = "Jose";
+cliente1.dniCliente = "13232";
+cliente1.numeroCuenta = "987897722";
+cliente1.saldoCuenta = 2000;
+
+const cliente2 =new cliente();
+cliente2.nombreCliente = "Leonardo";
+cliente2.dniCliente = "13232";
+cliente2.numeroCuenta = "987897722";
+cliente2.saldoCuenta = 2000;
+
+const cliente3 =new cliente();
+cliente3.nombreCliente = "Maria";
+cliente3.dniCliente = "132778";
+cliente3.numeroCuenta = "987822";
+cliente3.saldoCuenta = 2222;
+
+
 
 const nombreCliente = "leonardo";
 const dniCliente = "13804050";
@@ -13,3 +33,7 @@ const saldoCuenta = 1000;
 
 
 console.log(nombreCliente);
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
